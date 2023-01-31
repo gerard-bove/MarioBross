36d4adb483dd461e646ae8c24a7e7141ea7be882
Gerard Bove <gerardbovetous@gmail.com>
2023-01-31 13:08:46 +0100

    clases y colisiones

--- javascript.js
+++ javascript.js
@@ -8,33 +8,49 @@ window.onload = () => {
   const marioImage = new Image();
   marioImage.src = 'imagenes/Mario.png';
 
-  const enemiesImage = new Image();
-  enemiesImage.src = 'imagenes/Enemies.png'
+  const koopaImage = new Image();
+  koopaImage.src = 'imagenes/Enemies.png'
 
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
 
+  //koopa variables//
+  let xCutKoopa = 166;
+  let yCutKoopa = 0;
+  let wCutKoopa = 25;
+  let hCutKoopa = 30;
+  let xCanvasKoopa = 1100;
+  let xSpeedKoopa = 5;
+
+  //Mario variables//
+  let xCutMario = 500;
+  let yCutMario = 0;
+  let wCutMario = 30;
+  let hCutMario = 57;
+  let xCanvasMario = 500;
+  let xSpeedMario = 5;
+  let ySpeedMario = 5;
+
+  //game variables//
+  const enemiesArmy = [];
   let gameStarted = false;
   let jump = false;
   let jumpCount = 0;
   let enemiesCount = 0;
 
-  class Koopa {
-    constructor() {
-      this.img = enemiesImage;
-      this.xCut = 162;
-      this.yCut = 0;
-      this.wCut = 30;
-      this.hCut = 30;
-      this.xCanvas = 1100;
-      this.yCanvas = 484;
-      this.wCanvas = 35;
-      this.hCanvas = 40;
-      this.speed = 4;
-    }
-
-    moveLeft() {
-      this.xCanvas -= this.speed
+  class Character {  
+    constructor(img, xCut, yCut, wCut, hCut, xCanvas, wCanvas, hCanvas, xSpeed, ySpeed) {
+      this.img = img;
+      this.xCut = xCut;
+      this.yCut = yCut;
+      this.wCut = wCut;
+      this.hCut = hCut;
+      this.xCanvas = xCanvas;
+      this.yCanvas = 524 - hCanvas; 
+      this.wCanvas = wCanvas;
+      this.hCanvas = hCanvas;
+      this.xSpeed = xSpeed;
+      this.ySpeed = ySpeed
     }
 
     draw() {
@@ -42,57 +58,29 @@ window.onload = () => {
       ctx.drawImage(this.img, this.xCut, this.yCut, this.wCut, this.hCut, this.xCanvas, this.yCanvas, this.wCanvas, this.hCanvas);
     }
   }
-
-  class Mario{
+  class Koopa extends Character{
     constructor() {
-      this.img = marioImage;
-      this.xCut = 500;
-      this.yCut = 0;
-      this.wCut = 40;
-      this.hCut = 70;
-      this.xCanvas = 500;
-      this.yCanvas = 479;
-      this.wCanvas = 40;
-      this.hCanvas = 55;
-      this.speed = 5;
+      super(koopaImage, xCutKoopa, yCutKoopa, wCutKoopa, hCutKoopa, xCanvasKoopa, 35, 40, xSpeedKoopa)
+    }
+
+    moveLeft() {
+      this.xCanvas -= this.xSpeed;
+    }
+  }
+
+  class Mario extends Character{
+    constructor() {
+      super(marioImage, xCutMario, yCutMario, wCutMario, hCutMario, xCanvasMario, 28, 50, xSpeedMario, ySpeedMario)
     }
 
     moveUp() {
-      this.yCanvas -= this.speed;
+      this.yCanvas -= this.ySpeed;
     }
 
     moveDown() {
-      this.yCanvas += this.speed;
+      this.yCanvas += this.ySpeed;
     }
-
-    draw() {
-      //ctx.drawImage(imagenSprite, x_recorte, y_recorte, w_recorte, h_recorte, x_canvas, y_canvas, w_imagen, h_imagen);
-      ctx.drawImage(this.img, this.xCut, this.yCut, this.wCut, this.hCut, this.xCanvas, this.yCanvas, this.wCanvas, this.hCanvas);
-  }  
   }
-  
-  /* class Koopa extends Character {
-    constructor(yCut) {
-      super(yCut)
-      this.img = enemiesImage;
-      this.xCut = 162;
-      this.yCut = 0;
-      this.wCut = 30;
-      this.hCut = 30;
-      this.xCanvas = 1100;
-      this.yCanvas = 484;
-      this.wCanvas = 35;
-      this.hCanvas = 40;
-      this.speed = 4;
-    }
-    moveLeft() {
-      this.xCanvas -= this.speed
-    }
-    draw() {
-      //ctx.drawImage(imagenSprite, x_recorte, y_recorte, w_recorte, h_recorte, x_canvas, y_canvas, w_imagen, h_imagen);
-      ctx.drawImage(this.img, this.xCut, this.yCut, this.wCut, this.hCut, this.xCanvas, this.yCanvas, this.wCanvas, this.hCanvas);
-    }
-  } */
 
   const backgroundImage = {
     img: scenaryImage,
@@ -111,98 +99,94 @@ window.onload = () => {
       } else {
         ctx.drawImage(this.img, this.x - this.img.width, 0);
       }
-    },
-  };
-//hacer clase general para Mario y enemigos, y heredar de la general para marcar las caracteristicas de cada uno en su clase propia de herencia.
-//hacer clase juego que organice el jeugo y no este repartido.
-//controlar la eliminacion de enemigos del array con un delete(para machacar en memorias). 
-/*   const mario = {
-    img: marioImage,
-    x: 400,
-    y: 479,
-    speed: 0,
-
-
-    draw: function() {
-        //ctx.drawImage(imagenSprite, x_recorte, y_recorte, w_recorte, h_recorte, x_canvas, y_canvas, w_imagen, h_imagen);
-        ctx.drawImage(this.img, 500, 0, 40, 70, 500, this.y, 40, 55);
     }
-  } */
+  }
 
-  /* class Enemies {
-    constructor() {
-      this.img = enemiesImage;
-      this.xCut = 162;
-      this.yCut = 0;
-      this.wCut = 30;
-      this.hCut = 30;
-      this.xCanvas = 1100;
-      this.yCanvas = 484;
-      this.wCanvas = 35;
-      this.hCanvas = 40;
-      this.speed = 4;
+  class Game {
+    constructor(){
+      this.enemiesArmy = enemiesArmy;
+      this.gameStarted = gameStarted;
+      this.jump = jump;
+      this.jumpCount = jumpCount;
+      this.enemiesCount = enemiesCount;
+      this.identificator
     }
 
-    draw() {
-      //ctx.drawImage(imagenSprite, x_recorte, y_recorte, w_recorte, h_recorte, x_canvas, y_canvas, w_imagen, h_imagen);
-      ctx.drawImage(this.img, this.xCut, this.yCut, this.wCut, this.hCut, this.xCanvas, this.yCanvas, this.wCanvas, this.hCanvas);
+    jumpAction() {
+      if (this.jumpCount < 20) mario.moveUp();
+        if (this.jumpCount >= 20) mario.moveDown();
+        this.jumpCount ++;
+        if (this.jumpCount >= 40) {
+          this.jump = false;
+          this.jumpCount = 0;
+        }
     }
-  } */
+    
+    newEnemy() {
+      this.enemiesArmy.push(new Koopa());
+      this.enemiesCount = 0;
+    }
 
-  const enemiesArmy = [];
+    drawAll() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height); //borrar todo
+      backgroundImage.draw(); 
+      mario.draw();
+    }
+
+    gameOver() {
+      clearInterval(this.identificator)
+    }
+
+    update() {
+      backgroundImage.move();
+
+      if (this.jump) {   //Jump activado por la tecla flecha hacia arriba
+        this.jumpAction();
+      }
+
+      this.enemiesCount ++;
+      if (this.enemiesCount == 70) {
+        this.newEnemy();
+      }
+      if (this.enemiesArmy.length > 5) enemiesArmy.shift();   //eliminar los enemigos que ya han pasado por el escenario
+
+      this.drawAll();
+      
+      enemiesArmy.forEach((enemy) => {
+        enemy.moveLeft()  //desplazar la posición de todos los enemigos hacia la izquierda
+        enemy.draw();   //pintar toda la array de enemigos
+        if ( mario.xCanvas + mario.wCanvas > enemy.xCanvas && mario.xCanvas < enemy.xCanvas + enemy.wCanvas ) {
+          if ( mario.yCanvas + mario.hCanvas > enemy.yCanvas) this.gameOver();
+        }
+      });
+    }
+
+    startGame() {
+      gameStarted = true;
+      this.identificator = setInterval(()=> {
+        this.update();
+      }, 40)
+    }
+  }
+  
   let mario = new Mario();
   ////////Initial images///////////
 
 
   ////////Start game///////////
-  function startGame() {
-    gameStarted = true;
 
-    function update() {
-      backgroundImage.move();
-
-      if (jump) {   //Jump activado por la tecla flecha hacia arriba
-        if (jumpCount < 20) mario.moveUp();
-        if (jumpCount >= 20) mario.moveDown();
-        jumpCount ++;
-        if (jumpCount >= 40) {
-          jump = false;
-          jumpCount = 0;
-        }
-      }
-
-      enemiesCount ++;
-      if (enemiesCount == 70) {
-        enemiesArmy.push(new Koopa());
-        enemiesCount = 0;
-      }
-      if (enemiesArmy.length > 5) enemiesArmy.shift();
-
-      //repintar todo//
-      ctx.clearRect(0, 0, canvas.width, canvas.height); //borrar todo
-      backgroundImage.draw(); 
-      mario.draw();
-      enemiesArmy.forEach((enemy) => {
-        enemy.moveLeft()  //desplazar la posición de todos los enemigos hacia la izquierda
-        enemy.draw();   //pintar toda la array de enemigos
-      });
-    }
-
-     let identificator = setInterval(()=> {
-      update();
-    }, 40)
-  }
+  let game = new Game;
 
   ///////Manejo teclado//////////
   document.getElementsByTagName("body")[0].addEventListener("keydown", (event)=>{
     event.preventDefault();
     switch(event.key){
       case "ArrowUp":
-        if (!jump)
-        jump = true;
+        if (!game.jump)
+        game.jump = true;
         break;
       case ' ':
-        if (!gameStarted) startGame();
+        if (!gameStarted) game.startGame(); //la tecla "espacio" inicia el juego;
         break;
     }
   })  
